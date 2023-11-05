5b58446a8163ba85515222adbc642e1f52d2e491
Rakshitha037 <rakshitharakshitha6242@gmail.com>
2023-11-05 13:44:11 +0530

    Update baseUrl.js

--- frontend/src/utils/baseUrl.js
+++ frontend/src/utils/baseUrl.js
@@ -1,4 +1,12 @@
-const baseUrl = "http://localhost:3000"
+// const baseUrl = "http://localhost:3000"
+const baseURL = 'https://internship-voltix-backend.vercel.app';
+
+fetch(`${baseURL}/data`)
+    .then(response => response.json())
+    .then(data => {
+        // Handle the data from the backend
+    })
+    .catch(error => console.error('Error:', error));
 
 
 export default baseUrl

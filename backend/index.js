5ecae58dd7565f54584605bee22045210cb6f55d
Rakshitha037 <rakshitharakshitha6242@gmail.com>
2023-11-05 18:38:03 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ const app = express();
 //  // }
 //  ));
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://internship-voltix-frontend.vercel.app');
+  res.header('Access-Control-Allow-Origin', 'http://internship-voltix-frontend.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
